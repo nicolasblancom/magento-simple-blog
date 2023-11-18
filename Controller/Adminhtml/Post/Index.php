8fa1c4702b4ecbc7dfa2ddc1166a39311f966fbc
nicolasblancom <nicolasblancom@gmail.com>
2023-11-18 07:01:50 +0100

    Add admin post index controller

---
+++ Controller/Adminhtml/Post/Index.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace Nicolasblancom\Blog\Controller\Adminhtml\Post;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action implements HttpGetActionInterface
+{
+    const ADMIN_RESOURCE = 'Nicolasblancom_Blog::post';
+
+    /** @var PageFactory */
+    private $pageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        $page = $this->pageFactory->create();
+        $page->setActiveMenu('Nicolasblancom_Blog::post');
+        $page->getConfig()->getTitle()->prepend(__('Posts'));
+
+        return $page;
+    }
+}
